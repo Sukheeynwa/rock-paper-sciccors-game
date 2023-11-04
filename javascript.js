b7be288c6c944620bb5cc5c5171877594b33cb9a
Sukhee <sukheearch@gmail.com>
2023-11-04 11:53:01 +0800

    made javascript code much simpler

--- javascript.js
+++ javascript.js
@@ -5,72 +5,45 @@ let userChoice = "";
 let computerChoice = "";
 
 const choices = ["Rock", "Paper", "Sciccors"]
-// Create get user choice function
 
+// Create get user choice function
 function getUserChoice (i) {
     return choices[i];
 };
 
 // Create get computer choice function
-
 function getComputerChoice () {
     return choices[Math.floor(Math.random() * 3)];
 };
 
 // Create get result function
-
 function getResult (userInput, computerInput) {
-    if (userInput === "Rock" && computerInput === "Sciccors") {
-        userScore ++
-        result = "You won! Rock beats Sciccors"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result
-    } else if (userInput === "Paper" && computerInput === "Rock") {
-        userScore ++
-        result = "You won! Paper beats Rock"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result        
-    } else if (userInput === "Sciccors" && computerInput === "Paper") {
-        userScore ++
-        result = "You won! Sciccors beats Paper"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result        
-    } else if (computerInput === "Rock" && userInput === "Sciccors") {
-        computerScore ++
-        result = "You lose! Rock beats Sciccors"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result       
-    } else if (computerInput === "Paper" && userInput === "Rock") {
-        computerScore ++
-        result = "You lose! Paper beats Rock"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result     
-    } else if (computerInput === "Sciccors" && userInput === "Paper") {
-        computerScore ++
-        result = "You lose! Sciccors beats Paper"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result     
-    } else if (computerInput ===  userInput) {
+    if (userInput === "Rock" && computerInput === "Sciccors" || 
+        userInput === "Paper" && computerInput === "Rock" ||
+        userInput === "Sciccors" && computerInput === "Paper") {
+            userScore ++
+            result = `You won! ${userInput} beats ${computerInput}`
+    } else if (computerInput === "Rock" && userInput === "Sciccors" ||
+        computerInput === "Paper" && userInput === "Rock" ||
+        computerInput === "Sciccors" && userInput === "Paper") {
+            computerScore ++
+            result = `You lose! ${computerInput} beats ${userInput}`
+    } else {
         result = "Tied!"
-        userChoice = userInput
-        computerChoice = computerInput
-        return result        
     }; 
-          
+    userChoice = userInput
+    computerChoice = computerInput
 };
 
-
-
-function gameStart () {
-
+function gameStart (i) {
     getResult(getUserChoice(i), getComputerChoice())
-    
+
+    userScoreId.textContent = "User Score: " + userScore;
+    computerScoreId.textContent = "Computer Score: " + computerScore;
+    userChoiceId.textContent = "User Choice: " + userChoice;
+    computerChoiceId.textContent = "Computer Choice: " + computerChoice;
+    gameResult.textContent = "Result: " + result;
+
     if (userScore === 5) {
         alert("You won the game!")
     } else if (computerScore === 5) {
@@ -79,43 +52,31 @@ function gameStart () {
 }
 
 // Add event listeners to choice buttons
-
 let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let sciccors = document.querySelector("#sciccors");
-
 let userScoreId = document.querySelector("#user-score");
 let computerScoreId = document.querySelector("#computer-score");
 let gameResult = document.querySelector("#result");
 let userChoiceId = document.querySelector("#user-choice");
 let computerChoiceId = document.querySelector("#computer-choice");
 
+/* let btn = document.querySelectorAll("button")
+
+btn.forEach((button, i) => {
+    button.addEventListener("click", (i) => {
+        gameStart(i)
+    })
+}); */
+
 rock.addEventListener("click", () => {
-    i = 0;
-    gameStart()
-    userScoreId.textContent = "User Score: " + userScore;
-    computerScoreId.textContent = "Computer Score: " + computerScore;
-    userChoiceId.textContent = "User Choice: " + userChoice;
-    computerChoiceId.textContent = "Computer Choice: " + computerChoice;
-    gameResult.textContent = "Result: " + result;
+    gameStart(0)
 });
 
 paper.addEventListener("click", () => {
-    i = 1;
-    gameStart()
-    userScoreId.textContent = "User Score: " + userScore;
-    computerScoreId.textContent = "Computer Score: " + computerScore;
-    userChoiceId.textContent = "User Choice: " + userChoice;
-    computerChoiceId.textContent = "Computer Choice: " + computerChoice;
-    gameResult.textContent = "Result: " + result;
+    gameStart(1)
 });
 
 sciccors.addEventListener("click", () => {
-    i = 2;
-    gameStart()
-    userScoreId.textContent = "User Score: " + userScore;
-    computerScoreId.textContent = "Computer Score: " + computerScore;
-    userChoiceId.textContent = "User Choice: " + userChoice;
-    computerChoiceId.textContent = "Computer Choice: " + computerChoice;
-    gameResult.textContent = "Result: " + result;
+    gameStart(2)
 });
